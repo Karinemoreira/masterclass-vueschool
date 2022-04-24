3ab7f20872c9677943a0c1e9ac7214bfb7beab6b
Karine Moreira <karine.moreira@sealtelecom.com.br>
2022-04-24 19:04:35 -0300

    implemented posts-editor, cleaned project

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,38 +1,33 @@
 <template>
-  <div v-if="thread" class="col-large push-top">
-    <h1>{{ thread.title }}</h1>
+  <div class="post-list">
+    <div class="post" v-for="post in posts" :key="post.id">
+      <div class="user-info">
+        <a href="#" class="user-name">{{ userById(post.userId).name }}</a>
 
-    <div class="post-list">
-      <div class="post" v-for="postId in thread.posts" :key="postId">
-        <div class="user-info">
-          <a href="#" class="user-name">{{
-            userById(postById(postId).userId).name
-          }}</a>
+        <a href="#">
+          <img class="avatar-large" :src="userById(post.userId).avatar" alt=""
+        /></a>
 
-          <a href="#">
-            <img
-              class="avatar-large"
-              :src="userById(postById(postId).userId).avatar"
-              alt=""
-          /></a>
+        <p class="desktop-only text-small"></p>
+      </div>
 
-          <p class="desktop-only text-small"></p>
+      <div class="post-content">
+        <div>
+          <p>
+            {{ post.text }}
+          </p>
         </div>
+        <!--  <a href="#" style="margin-left: auto;" class="link-unstyled" title="Make a change"><i class="fa fa-pencil"></i></a> -->
+      </div>
 
-        <div class="post-content">
-          <div>
-            <p>
-              {{ postById(postId).text }}
-            </p>
-          </div>
-          <!--  <a href="#" style="margin-left: auto;" class="link-unstyled" title="Make a change"><i class="fa fa-pencil"></i></a> -->
-        </div>
+      <div
+        class="post-date text-faded"
+        :title="humanFriendlyDate(post.publishedAt)"
+      >
+        {{ diffForHumans(post.publishedAt) }}
+      </div>
 
-        <div class="post-date text-faded">
-          {{ postById(postId).publishedAt }}
-        </div>
-
-        <!--  <div class="reactions">
+      <!--  <div class="reactions">
                 <ul>
                     <li></li>
                     <li></li>
@@ -42,41 +37,39 @@
                     <li></li>
                 </ul>
             </div> -->
-      </div>
     </div>
   </div>
-  <div v-else class="col-full text-center">
-    <h1>This thread does not exist</h1>
-    <router-link :to="{ name: 'Home' }">Read some cool threads</router-link>
-  </div>
 </template>
+
 <script>
   import data from "@/data.json";
+  import dayjs from "dayjs";
+  import relativeTime from "dayjs/plugin/relativeTime";
+  import localizedDate from "dayjs/plugin/localizedFormat";
+  dayjs.extend(relativeTime);
+  dayjs.extend(localizedDate);
+
   export default {
     props: {
-      id: {
+      posts: {
         required: true,
-        type: String,
+        type: Array,
       },
     },
     data() {
       return {
-        threads: data.threads,
-        posts: data.posts,
         users: data.users,
       };
     },
-    computed: {
-      thread() {
-        return this.threads.find((thread) => thread.id === this.id);
-      },
-    },
     methods: {
-      postById(postId) {
-        return this.posts.find((p) => p.id === postId);
-      },
       userById(userId) {
-        return this.users.find((u) => u.id === userId);
+        return this.users.find((p) => p.id === userId);
+      },
+      diffForHumans(timestamp) {
+        return dayjs.unix(timestamp).fromNow();
+      },
+      humanFriendlyDate(timestamp) {
+        return dayjs.unix(timestamp).format(" lll");
       },
     },
   };
